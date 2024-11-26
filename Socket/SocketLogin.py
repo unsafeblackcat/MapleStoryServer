00d86daf106234fd2df97db538c4dc0d627a4715
unsafeblackcat <unsafeblackcat@gmail.com>
2024-11-26 23:33:02 +0800

    调整下socket接口函数

--- Socket/SocketLogin.py
+++ Socket/SocketLogin.py
@@ -6,13 +6,30 @@ class SocketLogin(SocketBase):
         super().__init__(ip, port, time_out)
         pass
  
-    # 返回值: 1, 如果需要给客户端返回数据包
-    # 返回值: 0, 不需要给客户端返回数据包
-    async def socket_connect(self, addr:tuple, output_data: bytearray = None) -> int:
+    async def socket_connect(self, addr:tuple, output_data:bytearray) -> int:
+        '''
+            当客户端建立连接请求时会进入此回调
+
+            addr: 客户端连接IP和端口
+            output_data: 建立连接请求时如果需发送数据包给客户端从此处返回
+
+            返回值: int类型
+                0: 继续监听客户端过来的数据包
+                1: 拒绝客户端建立连接, 将主动断开客户端接入的TCP请求
+        '''
         return 0
 
-    # 返回值: 1, 如果需要给客户端返回数据包
-    # 返回值: 0, 不需要给客户端返回数据包
-    async def socket_callback(self, addr:tuple, input_data:bytes = None, output_data: bytearray = None) -> int:
+    async def socket_callback(self, addr:tuple, input_data:bytes, output_data:bytearray) -> int:
+        '''
+            当收到客户端数据包时会进入此回调
+
+            addr: 客户端连接IP和端口
+            intpu_data: 客户端发送过来的数据包
+            output_data: 建立连接请求时如果需发送数据包给客户端从此处返回
+
+            返回值: int类型
+                0: 继续监听客户端过来的数据包
+                1: 拒绝客户端建继续连接, 将主动断开客户端接入的TCP请求
+        '''
         return
     
